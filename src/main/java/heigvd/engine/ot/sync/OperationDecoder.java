2bb81bf6e95c954d7162bfed0687178be9b517c4
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-02-25 01:09:10 +0100

    Add some identity helpers for encoding and decoding functions

--- src/main/java/heigvd/engine/ot/sync/OperationDecoder.java
+++ src/main/java/heigvd/engine/ot/sync/OperationDecoder.java
@@ -2,6 +2,10 @@ package heigvd.engine.ot.sync;
 
 public interface OperationDecoder<T, Encoding> {
 
+  static <T> OperationDecoder<T, T> identity() {
+    return t -> t;
+  }
+
   T decode(Encoding encoding);
 
 }
--- src/main/java/heigvd/engine/ot/sync/OperationEncoder.java
+++ src/main/java/heigvd/engine/ot/sync/OperationEncoder.java
@@ -2,6 +2,10 @@ package heigvd.engine.ot.sync;
 
 public interface OperationEncoder<T, Encoding> {
 
+  static <T> OperationEncoder<T, T> identity() {
+    return t -> t;
+  }
+
   Encoding encode(T operation);
 
 }
